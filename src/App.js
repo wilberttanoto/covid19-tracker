216cdfb3cdcdf7189bddde1cb12848495d4c2da0
Wilbert Tanoto <wilbert.tanoto@webimp.com.sg>
2020-04-30 16:30:58 +0700

    Added material ui, chart and connecting API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+
+import styles from './App.module.css';
+import { Typography } from '@material-ui/core';
+
+import { fetchData } from './api/'; // auto go to index.js
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData(this.state.country);
+        this.setState({data: fetchedData});
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({data: fetchedData, country: country});
+    }
+
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <Typography variant="h5">
+                    Visualized from awesome API in
+                    <a href="https://covid19.mathdro.id/api/"> Mathdro API </a>
+                </Typography>  
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country} />
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// So in App.js component can be imported tidier
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
